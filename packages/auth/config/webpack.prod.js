f66353e393e3841f12bab6d8c53347f1e56755c5
gustavobgt <gustavo_bgt97@hotmail.com>
2022-12-21 07:50:15 -0300

    feat: add auth app feature

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -3,24 +3,22 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
-const devConfig = {
-  mode: "development",
+const prodConfig = {
+  mode: "production",
   output: {
-    publicPath: 'http://localhost:8080/'
-  },
-  devServer: {
-    port: 8080,
-    historyApiFallback: true,
+    filename: "[name].[contenthash].js",
+    publicPath: '/auth/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      name: "auth",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./AuthApp": "./src/bootstrap.js",
       },
       shared: packageJson.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import AuthApp from "./components/AuthApp";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -18,7 +19,10 @@ const App = () => {
         <div>
           <Header />
 
-          <MarketingApp />
+          <Switch>
+            <Route exact path="/auth" component={AuthApp} />
+            <Route exact path="/" component={MarketingApp} />
+          </Switch>
         </div>
       </StylesProvider>
     </BrowserRouter>

8b23f1fd46d5a42edc72b8d26f28648a28093a4c
gustavobgt <gustavo_bgt97@hotmail.com>
2022-12-22 07:45:59 -0300

    feat: add dashboard feature

---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,13 @@
+import { mount } from "dashboard/DashboardApp";
+
+const DashboardApp = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
+
+export default DashboardApp;
